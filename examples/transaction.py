53d64f1ebd12ea190d655e12b105a41823333846
Maanas Royy <maanas@maanas.co>
2015-04-16 00:16:29 +0530

    Fixed setup.py to include subpackages

--- examples/transaction.py
+++ examples/transaction.py
@@ -6,24 +6,15 @@ agms.Configuration.init('init.ini')
 
 trans = agms.Transaction()
 
-# params = {
-#     'transaction_type': {'value': 'sale'},
-#     'amount': {'value': '100.00'},
-#     'cc_number': {'value': '4111111111111111'},
-#     'cc_exp_date': {'value': '0520'},
-#     'cc_cvv': {'value': '123'}
-# }
-#
-# result = trans.process(params)
-#
-# print result
-
 params = {
-    'transaction_type': {'value': 'void'},
-    'transaction_id': {'value': '123'}
+    'transaction_type': {'value': 'sale'},
+    'amount': {'value': '100.00'},
+    'cc_number': {'value': '4111111111111111'},
+    'cc_exp_date': {'value': '0520'},
+    'cc_cvv': {'value': '123'}
 }
 
-try:
-    transaction_result = trans.process(params)
-except Exception, err:
-    print err[1]
+result = trans.process(params)
+
+print result
+
