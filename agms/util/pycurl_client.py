20a113be8a055dbe777fee47fc77f0c8c0787516
Maanas Royy <maanas@maanas.co>
2015-04-20 06:56:57 +0530

    Updated curl options

--- agms/util/pycurl_client.py
+++ agms/util/pycurl_client.py
@@ -15,38 +15,36 @@ class PycurlClient(object):
     def http_do(self, http_verb, url, headers, request_body):
         curl = pycurl.Curl()
         response = StringIO()
-
-        
-        curl.setopt(pycurl.CAINFO, True)
-        curl.setopt(pycurl.SSL_VERIFYPEER, 1)
-        curl.setopt(pycurl.SSL_VERIFYHOST, 2)
-        curl.setopt(pycurl.URL, url)
-        curl.setopt(pycurl.ENCODING, 'gzip')
-        curl.setopt(pycurl.WRITEFUNCTION, response.write)
-        curl.setopt(pycurl.FOLLOWLOCATION, 1)
-        curl.setopt(pycurl.HTTPHEADER, self._format_headers(headers))
+        curl.setopt(curl.CAINFO, True)
+        curl.setopt(curl.SSL_VERIFYPEER, 1)
+        curl.setopt(curl.SSL_VERIFYHOST, 2)
+        curl.setopt(curl.URL, url)
+        curl.setopt(curl.ENCODING, 'gzip')
+        curl.setopt(curl.WRITEFUNCTION, response.write)
+        curl.setopt(curl.FOLLOWLOCATION, 1)
+        curl.setopt(curl.HTTPHEADER, self._format_headers(headers))
         self._set_request_method_and_body(curl, http_verb, request_body)
 
         curl.perform()
 
-        status = curl.getinfo(pycurl.HTTP_CODE)
+        status = curl.getinfo(curl.HTTP_CODE)
         response = response.getvalue()
         return [status, response]
 
     def _set_request_method_and_body(self, curl, method, body):
         if method == "GET":
-            curl.setopt(pycurl.HTTPGET, 1)
+            curl.setopt(curl.HTTPGET, 1)
         elif method == "POST":
-            curl.setopt(pycurl.POST, 1)
-            curl.setopt(pycurl.POSTFIELDSIZE, len(body))
+            curl.setopt(curl.POST, 1)
+            curl.setopt(curl.POSTFIELDSIZE, len(body))
         elif method == "PUT":
-            curl.setopt(pycurl.PUT, 1)
-            curl.setopt(pycurl.INFILESIZE, len(body))
+            curl.setopt(curl.PUT, 1)
+            curl.setopt(curl.INFILESIZE, len(body))
         elif method == "DELETE":
             curl.setopt(curl.CUSTOMREQUEST, "DELETE")
 
         if body:
-            curl.setopt(pycurl.READFUNCTION, StringIO.StringIO(body).read)
+            curl.setopt(curl.READFUNCTION, StringIO(body).read)
 
     def _format_headers(self, headers):
         return [key + ": " + value for key, value in headers.items()]
